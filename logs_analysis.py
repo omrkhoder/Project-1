8770f3b361bb5267b5c81eee5e1ba814d4cc59e9
omrkhoder <49230362+omrkhoder@users.noreply.github.com>
2019-07-20 16:11:06 +0200

    Update logs_analysis.py

--- logs_analysis.py
+++ logs_analysis.py
@@ -12,36 +12,43 @@ def connect(query):
     conn.close()
     return results
 
-# Print the top three articles of all time
+# 1- Print the top three articles of all time:
 
 def top_three_articles():
-    results = connect("""SELECT * FROM article_views LIMIT 3;""")
-    print('\n Displaying the most popular articles of all time:\n')
+    results = connect("""select articles.title, count(*) as views 
+            from log, articles
+            where log.status='200 OK' 
+            and articles.slug = substr(log.path, 10)
+            group by articles.title
+            order by views desc
+            limit 3;""")
+    print('\n Displaying the top three articles of all time:\n')
     for i in results:
         print('\t' + str(i[0]) + ' - ' + str(i[1]) + ' views')
         print(" ")
 
 
-# Print the most popular article authors of all time
+# 2- Print the most popular article authors of all time:
 def top_authors():
-    results = connect("""SELECT name, sum(article_views.views) AS views
-            FROM article_authors, article_views
-            WHERE article_authors.title = article_views.title
-            GROUP BY name
-            ORDER BY views desc;""")
-    print('\n Displaying the most popular authors of all time:\n')
+    results = connect("""select authors.name, sum(article_views.views) as views
+            from article_authors, article_views,log
+            where log.status='200 OK'
+            and article_authors.title = article_views.title
+            group by authors.name
+            order by views desc;""")
+    print('\n Displaying the most popular article authors of all time:\n')
     for i in results:
         print('\t' + str(i[0]) + ' - ' + str(i[1]) + ' views')
         print(" ")
 
 
-# Print the days in which there were more than 1% of requests lead to errors
+# 3-Print the days in which there were more than 1% of requests lead to errors
 def error_percentage():
-    results = connect("""SELECT errorlogs.date, round(100.0*errorcount/logcount,2) as percent
-            FROM logs, errorlogs
-            WHERE logs.date = errorlogs.date
-            AND errorcount > logcount/100;""")
-    print('\n The days when more than 1% of requests lead to error:\n')
+    results = connect("""select errorlogs.date, round(100.0*errorcount/logcount,2) as percent
+            from logs, errorlogs
+            where logs.date = errorlogs.date
+            and errorcount > logcount/100;""")
+    print('\n The days when more than 1% of requests lead to errors:\n')
     for i in results:
         print('\t' + str(i[0]) + ' - ' + str(i[1]) + ' %' + ' errors')
         print(" ")
@@ -55,5 +62,7 @@ if __name__ == '__main__':
     top_three_articles()
     top_three_authors()
     high_error_days()
+
+
 
 
